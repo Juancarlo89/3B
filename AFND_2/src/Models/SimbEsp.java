c35a93d4cc04ffe363d3bde9a152e72c9144a451
Juancarlo89 <93235805+Juancarlo89@users.noreply.github.com>
2025-10-23 10:21:28 -0600

    Vamos a eliminar todas las carpetas
    ahora hay que esperar a que se trabaje con la carpeta de pao

--- AFND_2/src/Models/SimbEsp.java
+++
@@ -1,9 +0,0 @@
-package Models;
-
-public final class SimbEsp {
-    public static final char EPSILON = (char) 5;
-    public static final char FIN = (char) 0;
-    public static final int ERROR = 2000;
-    public static final int OMITIR = 2001;
-    private SimbEsp() { throw new AssertionError("No instanciar."); }
-}
